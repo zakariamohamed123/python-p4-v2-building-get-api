03346d0d3762f6f1cc3bcd93bdc1d75598478758
zakariamohamed123 <zakaria.mohamed1@student.moringaschool.com>
2024-04-15 13:23:08 +0300

    complete

---
+++ server/app.py
@@ -0,0 +1,27 @@
+# server/app.py
+
+from flask import Flask, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from models import db, User, Review, Game
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+@app.route('/')
+def index():
+    return "Index for Game/Review/User API"
+
+# start building your API here
+
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
+
